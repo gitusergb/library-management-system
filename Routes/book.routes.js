c0726bcfa24fc315bd9d1cd5bee04bd447dfed84
gitusergb <g4ur131@gmail.com>
2024-09-10 20:57:00 +0530

    bms

--- Routes/book.routes.js
+++ Routes/book.routes.js
@@ -5,13 +5,13 @@ const {role} = require('../middleware/role.middleware');
 
 const bookRouter= express.Router();
 //restricted
-bookRouter.post('/addbook',auth,role(['admin']), createbook);
+bookRouter.post('/addbook', createbook);
 bookRouter.get('/',seebook);
 bookRouter.get(`/:bookIsbn`,auth,role(['admin','author']),byisbn);//get by ISBN
 bookRouter.get('/:author',authorgetbook);
-bookRouter.get('/:bookID',auth,idgetbook);//get by ID
+bookRouter.get('/:bookID',idgetbook);//get by ID
 bookRouter.patch('/update/:bookID',auth,role(['admin','author']),updatebook);
-bookRouter.delete('/delete/:bookID',auth,role(['admin','author']),deletebook);
+bookRouter.delete('/delete/:bookID',deletebook);
 /////////////////////////GB////////////////////////////
 
 module.exports = {bookRouter};
